adc2d6ce6ed76563128a1146ebde10f0297a4884
Priit Tammets <tammets@gmail.com>
2024-11-12 10:41:24 +0200

    new map page and layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    theme: {
+      extend: {
+        colors: {
+          primary: 'rgb(0, 170, 161)', // Custom color for buttons
+          'primary-hover': 'rgb(0, 150, 143)', // Darker shade for hover
+        }
+      },
+    },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
 
